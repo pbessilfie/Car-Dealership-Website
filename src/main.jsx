7c3a7ec65f016a62df5245eac265520b6b076daa
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-07-21 17:16:52 +0000

    added routing paths to all the pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,22 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import Browse from "./Pages/Browse.jsx";
+import Search from "./Pages/Search.jsx";
+import Profile from "./Pages/Profile.jsx";
+import Help from "./Pages/Help.jsx";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App />, children: [{ path: "", element: "" }] },
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/browse", element: <Browse /> },
+      { path: "/search", element: <Search /> },
+      { path: "/profile", element: <Profile /> },
+      { path: "/help", element: <Help /> },
+    ],
+  },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
