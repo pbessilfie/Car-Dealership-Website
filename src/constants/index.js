83f5e3bea7e03bd48cd411398436a004cb8fb9e9
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-07-21 17:14:40 +0000

    added footerlinks object

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,17 +1,51 @@
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa6";
+
 export const Navlinks = [
-    {label:'Browse',
-        path:'/browse'
-    },
-    {label:'Search',
-        path:'/search'
-    },
-    {label:'Profile',
-        path:'/profile'
-    },
-    {label:'Help',
-        path:'/help'
-    },
-    // {label:'Browse',
-    //     path:'/browse'
-    // },
-]
+  { label: "Browse", path: "/browse" },
+  { label: "Search", path: "/search" },
+  { label: "Profile", path: "/profile" },
+  { label: "Help", path: "/help" },
+  // {label:'Browse',
+  //     path:'/browse'
+  // },
+];
+export const Footerlinks = [
+  {
+    title: "Company",
+    links: [
+      { label: "About", path: "" },
+      { label: "How it works", path: "" },
+      { label: "Coperation", path: "" },
+    ],
+  },
+  {
+    title: "Support",
+    links: [
+      { label: "Community", path: "" },
+      { label: "Livechat", path: "" },
+      { label: "FAQ", path: "" },
+    ],
+  },
+  {
+    title: "Links",
+    links: [
+      { label: "Help & Support", path: "" },
+      { label: "Terms & Privacy", path: "" },
+      { label: "Cookies", path: "" },
+    ],
+  },
+  {
+    title: "Location",
+    links: [
+      { label: "Kwabenya - Pokuasi RD", path: "" },
+      { label: "Ghana, Greater - Accra", path: "" },
+    ],
+  },
+  {
+    title: "Connect",
+    links: [
+      { label: "info@logo.com", path: "" },
+      { label: "(233) 248 503 913", path: "" },
+    ],
+  },
+];
