2158ad62c9fd4d63dedc12cb438da09e5c25b828
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-07-21 17:13:17 +0000

    footer for large devices

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,52 @@
+import { Footerlinks } from "../constants";
+import { FaRegCopyright, FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-[#31373e] flex flex-col items-center justify-center py-24">
+      <div className="">
+        <div className=" flex gap-40 border-b border-secondaryTextColor pb-28">
+          {Footerlinks.map((item) => (
+            <div key={item.title} className="">
+              <h4 className="text-white text-xl font-medium py-4">
+                {item.title}
+              </h4>
+              <ul>
+                {item.links.map((link, index) => (
+                  <li
+                    key={index}
+                    className=" text-base font-normal text-secondaryTextColor2 hover:text-secondaryTextColor cursor-pointer mb-1"
+                  >
+                    {link.label}
+                  </li>
+                ))}
+              </ul>
+              {item.title === "Connect" && (
+                <div className="flex w-full justify-between">
+                  <div className=" p-2 bg-secondaryTextColor text-white rounded-full cursor-pointer text-lg">
+                    <FaFacebookF />
+                  </div>
+                  <div className=" p-2 bg-secondaryTextColor text-white rounded-full cursor-pointer text-lg">
+                    <FaTwitter />
+                  </div>
+                  <div className=" p-2 bg-secondaryTextColor text-white rounded-full cursor-pointer text-lg">
+                    <FaInstagram />
+                  </div>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-between py-4 px-6">
+          <span className=" text-3xl font-bold text-white">LOGO</span>
+          <span className="flex gap-2 items-center text-white text-sm font-light">
+            <FaRegCopyright />
+            2024 Logo LTD. All Rights Reserved
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
