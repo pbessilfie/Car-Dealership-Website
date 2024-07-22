d87db95d0d95ebcd912ed5fdb14879af289a65b8
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-07-22 23:04:16 +0000

    created a component to render all recently added cars details

---
+++ src/Components/RecentlyAdded.jsx
@@ -0,0 +1,32 @@
+import { Cars } from "../constants";
+import Car_Card from "./Car_Card";
+
+const RecentlyAdded = () => {
+  return (
+    <div className="">
+      <h3 className=" text-primaryTextColor font-bold text-xl mb-2">
+        Recently Added
+      </h3>
+      <p className="text-sm text-secondaryTextColor font-normal mb-9">
+        Discover all recently added cars
+      </p>
+      <div className="flex gap-5">
+        {Cars.slice(5, 10).map((car, index) => (
+          <Car_Card
+            key={index}
+            carName={car.Carname}
+            price={car.price}
+            condition={car.condition}
+            production_year={car.production_year}
+            reviews={car.reviews}
+            brand={car.brand}
+            mileage={car.mileage}
+            category={car.category}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default RecentlyAdded
