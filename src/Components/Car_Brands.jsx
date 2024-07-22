97ab1cf28ba51bae2d26e181a851874f9a0eacae
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-07-22 23:02:15 +0000

    component displaying the various car brands

---
+++ src/Components/Car_Brands.jsx
@@ -0,0 +1,95 @@
+const Car_Brands = () => {
+  return (
+    <div className=" py-7  flex justify-center items-center mt-20">
+      <div>
+        <h3 className=" text-primaryTextColor font-bold text-xl mb-6">
+          Popular Brands
+        </h3>
+        <div className="flex gap-6">
+          <div className="bg-white rounded-md drop-shadow-md w-28 h-24 flex flex-col justify-evenly items-center">
+            <img
+              src="/src/assets/bmw_of_north_america_logo.svg.png"
+              width={50}
+              className=""
+            />
+            <span className=" text-base text-primaryTextColor font-normal ">
+              129
+            </span>
+          </div>
+          <div className="bg-white rounded-md drop-shadow-md w-28 h-24 flex flex-col justify-evenly items-center">
+            <img
+              src="/src/assets/toyota_symbol.svg.png"
+              width={80}
+              className=""
+            />
+            <span className=" text-base text-primaryTextColor font-normal ">
+              129
+            </span>
+          </div>
+          <div className="bg-white rounded-md drop-shadow-md w-28 h-24 flex flex-col justify-evenly items-center">
+            <img
+              src="/src/assets/Group.png"
+              width={50}
+              className=""
+            />
+            <span className=" text-base text-primaryTextColor font-normal ">
+              129
+            </span>
+          </div>
+          <div className="bg-white rounded-md drop-shadow-md w-28 h-24 flex flex-col justify-evenly items-center">
+            <img
+              src="/src/assets/mercedes-benz_symbol.svg.png"
+              width={50}
+              className=""
+            />
+            <span className=" text-base text-primaryTextColor font-normal ">
+              129
+            </span>
+          </div>
+          <div className="bg-white rounded-md drop-shadow-md w-28 h-24 flex flex-col justify-evenly items-center">
+            <img
+              src="/src/assets/hyundai_symbol.svg.png"
+              width={50}
+              className=""
+            />
+            <span className=" text-base text-primaryTextColor font-normal ">
+              129
+            </span>
+          </div>
+          <div className="bg-white rounded-md drop-shadow-md w-28 h-24 flex flex-col justify-evenly items-center">
+            <img
+              src="/src/assets/audi_logo.svg.png"
+              width={50}
+              className=""
+            />
+            <span className=" text-base text-primaryTextColor font-normal ">
+              129
+            </span>
+          </div>
+          <div className="bg-white rounded-md drop-shadow-md w-28 h-24 flex flex-col justify-evenly items-center">
+            <img
+              src="/src/assets/lexus_logo.svg.png"
+              width={90}
+              className=""
+            />
+            <span className=" text-base text-primaryTextColor font-normal ">
+              129
+            </span>
+          </div>
+          <div className="bg-white rounded-md drop-shadow-md w-28 h-24 flex flex-col justify-evenly items-center">
+            <img
+              src="/src/assets/honda_symbol.svg.png"
+              width={50}
+              className=""
+            />
+            <span className=" text-base text-primaryTextColor font-normal ">
+              129
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Car_Brands;
