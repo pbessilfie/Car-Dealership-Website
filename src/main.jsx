ac2d0d0661f62baac559213a8342a4e4adee101d
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-07-23 21:24:01 +0000

    added routes to various category components

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,37 @@ import Browse from "./Pages/Browse.jsx";
 import Search from "./Pages/Search.jsx";
 import Profile from "./Pages/Profile.jsx";
 import Help from "./Pages/Help.jsx";
+import Browse_Categories from "./Pages/Browse_Categories.jsx";
+import Hatchbacks from "./Pages/Hatchbacks.jsx";
+import Sedan from "./Pages/Sedan.jsx";
+import Trucks from "./Pages/Trucks.jsx";
+import Van from "./Pages/Van.jsx";
+import Wagon from "./Pages/Wagon.jsx";
+import Suv from "./Pages/Suv.jsx";
+import Coupe from "./Pages/Coupe.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { path: "/browse", element: <Browse /> },
+      {
+        path: "/browse",
+        element: <Browse />,
+      },
+      {
+        path: "/browse/categories",
+        element: <Browse_Categories />,
+        children: [
+          { path: "/browse/categories/hatchbacks", element: <Hatchbacks /> },
+          { path: "/browse/categories/sedans", element: <Sedan /> },
+          { path: "/browse/categories/coupes", element: <Coupe /> },
+          { path: "/browse/categories/suvs", element: <Suv /> },
+          { path: "/browse/categories/wagons", element: <Wagon /> },
+          { path: "/browse/categories/vans", element: <Van /> },
+          { path: "/browse/categories/trucks", element: <Trucks /> },
+        ],
+      },
       { path: "/search", element: <Search /> },
       { path: "/profile", element: <Profile /> },
       { path: "/help", element: <Help /> },
