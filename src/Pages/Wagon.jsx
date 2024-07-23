778ff6a6ed0d4445e7eb4275dfb95e67cbf3932e
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-07-23 21:18:07 +0000

    created a component to render all wagon category vehicles

---
+++ src/Pages/Wagon.jsx
@@ -0,0 +1,26 @@
+import Category_Card from "../Components/Category_Card";
+import { Cars } from "../constants";
+
+const Wagon = () => {
+  return (
+    <div className=" mt-[3%]">
+      <div className="grid grid-cols-3 gap-x-5 gap-y-8 place-items-center px-44">
+        {Cars.filter((car) => car.category == "Wagon").map((car, index) => (
+          <Category_Card
+            key={index}
+            carName={car.Carname}
+            price={car.price}
+            condition={car.condition}
+            production_year={car.production_year}
+            reviews={car.reviews}
+            brand={car.brand}
+            mileage={car.mileage}
+            category={car.category}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Wagon
