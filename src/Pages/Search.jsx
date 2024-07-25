6a3dfb826497cdb4bde15adf53e943219e821982
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-07-25 02:11:44 +0000

    where specific vehicle is searched

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,8 +1,24 @@
+import FilterSidebar from "../Components/FilterSidebar";
+import Search_Result from "../Components/Search_Result";
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className=" bg-mainBGC">
+      <div className=" bg-white py-9 text-center space-y-3">
+        <h1 className=" text-3xl font-bold text-primaryTextColor">LOGO CARS</h1>
+        <p className=" text-base text-secondaryTextColor font-normal">
+          Find your perfect vehicle, narrow it down by price or whatever you
+          want
+        </p>
+      </div>
 
-export default Search
+      {/* searched results */}
+      <div className=" py-12 flex gap-12 justify-center">
+        <FilterSidebar/>
+        <Search_Result/>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
