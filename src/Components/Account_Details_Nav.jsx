a771925bb319a1d79ffb51ec3f68a29030e37b27
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-07-26 23:59:10 +0000

    changing the state of the active nav when a new one is clicked

--- src/Components/Account_Details_Nav.jsx
+++ src/Components/Account_Details_Nav.jsx
@@ -2,8 +2,44 @@ import { FaUser } from "react-icons/fa6";
 import { IoMdLock } from "react-icons/io";
 import { MdCardMembership } from "react-icons/md";
 import { PiSignOutBold } from "react-icons/pi";
+import PropTypes from "prop-types";
 
-const Account_Details_Nav = () => {
+const Account_Details_Nav = ({ activeSN, setActiveSN }) => {
+  const navItems = [
+    {
+      label: "Account Information",
+      icon: (
+        <div className=" rounded-full text-white text-2xl p-2 bg-[#D273EA]">
+          <FaUser />
+        </div>
+      ),
+    },
+    {
+      label: "Security",
+      icon: (
+        <div className=" rounded-full text-white text-2xl p-2 bg-pBlue">
+          <IoMdLock />
+        </div>
+      ),
+    },
+    {
+      label: "Membership Settings",
+      icon: (
+        <div className=" rounded-full text-white text-2xl p-2 bg-[#21BB9B]">
+          <MdCardMembership />
+        </div>
+      ),
+    },
+    {
+      label: "Sign out",
+      icon: (
+        <div className=" rounded-full text-white text-2xl p-2 bg-pOrange">
+          <PiSignOutBold />
+        </div>
+      ),
+    },
+  ];
+
   return (
     <div className=" bg-white rounded-lg drop-shadow-md w-[300px]">
       <div className=" p-5 border-b border-lineColor">
@@ -13,40 +49,25 @@ const Account_Details_Nav = () => {
         </h2>
       </div>
 
-      <div className=" flex items-center gap-3 bg-white hover:bg-[#eee] p-4 hover:border-l-4 hover:border-pBlue">
-        <div className=" rounded-full text-white text-2xl p-2 bg-[#D273EA]">
-          <FaUser />
+      {navItems.map((item, index) => (
+        <div
+          key={index}
+          className={`flex items-center gap-3 bg-white hover:bg-[#eee] p-4 hover:border-l-4 hover:border-pBlue cursor-pointer ${
+            activeSN === item.label && "border-l-4 border-pBlue bg-[#eee]"
+          }`}
+          onClick={() => setActiveSN(item.label)}
+        >
+          {item.icon}{" "}
+          <span className=" text-primaryTextColor text-base font-medium">
+            {item.label}
+          </span>
         </div>
-        <span className=" text-primaryTextColor text-base font-medium">
-          Account Information
-        </span>
-      </div>
-      <div className=" flex items-center gap-3 bg-white hover:bg-[#eee] p-4 hover:border-l-4 hover:border-pBlue">
-        <div className=" rounded-full text-white text-2xl p-2 bg-pBlue">
-          <IoMdLock />
-        </div>
-        <span className=" text-primaryTextColor text-base font-medium">
-          Security
-        </span>
-      </div>
-      <div className=" flex items-center gap-3 bg-white hover:bg-[#eee] p-4 hover:border-l-4 hover:border-pBlue">
-        <div className=" rounded-full text-white text-2xl p-2 bg-[#21BB9B]">
-          <MdCardMembership />
-        </div>
-        <span className=" text-primaryTextColor text-base font-medium">
-          Membership
-        </span>
-      </div>
-      <div className=" flex items-center gap-3 bg-white hover:bg-[#eee] p-4 hover:border-l-4 hover:border-pBlue">
-        <div className=" rounded-full text-white text-2xl p-2 bg-pOrange">
-          <PiSignOutBold />
-        </div>
-        <span className=" text-primaryTextColor text-base font-medium">
-          Sign out
-        </span>
-      </div>
+      ))}
     </div>
   );
 };
-
+Account_Details_Nav.propTypes = {
+  activeSN: PropTypes.string,
+  setActiveSN: PropTypes.func,
+};
 export default Account_Details_Nav;
