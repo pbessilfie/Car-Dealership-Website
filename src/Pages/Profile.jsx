bd7e7d67c16519c497185071fa33988425e0aed1
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-07-26 19:38:26 +0000

    profile details

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,8 +1,14 @@
+import Account_Details_Nav from "../Components/Account_Details_Nav";
+import Personal_Info from "../Components/Personal_Info";
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className=" bg-mainBGC flex items-start justify-center gap-14 py-28">
+      
+      <Account_Details_Nav />
+      <Personal_Info />
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
