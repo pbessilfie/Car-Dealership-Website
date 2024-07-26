aedd7dd679a4f336775eb940fc9c850b6421ebf7
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-07-26 19:34:59 +0000

    modified it to take icon and type prop

--- src/Components/Select_Input.jsx
+++ src/Components/Select_Input.jsx
@@ -1,11 +1,21 @@
 import { FaChevronDown, FaChevronUp } from "react-icons/fa6";
 import PropTypes from "prop-types";
-const Select_Input = ({placeholder}) => {
-  
+const Select_Input = ({placeholder,type, icon}) => {
   return (
     <div className=" w-full h-10 rounded-lg border border-lineColor flex">
+      {icon && (
+        <div className="p-2 h-full grid place-items-center">
+          <img
+            src="/src/assets/usa-flag.png"
+            alt="country flag"
+            width={25}
+            className="bg-white my-auto"
+          />{" "}
+        </div>
+      )}
+
       <input
-        type="text"
+        type={type}
         placeholder={placeholder}
         className=" h-full bg-transparent outline-none border-none placeholder:text-base placeholder:font-medium placeholder:text-secondaryTextColor2 p-2 flex-1 text-primaryTextColor"
       />
@@ -22,6 +32,8 @@ const Select_Input = ({placeholder}) => {
 
 Select_Input.propTypes = {
   placeholder: PropTypes.string,
+  type: PropTypes.string,
+  icon: PropTypes.bool,
 };
 
 export default Select_Input
