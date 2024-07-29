8fa557c437ac4adfebfcaf28c05c58135b69d104
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-07-29 19:12:16 +0000

    added two-step verification

--- src/Components/Popups/Verification.jsx
+++ src/Components/Popups/Verification.jsx
@@ -1,8 +1,108 @@
+import { LiaTimesCircleSolid } from "react-icons/lia";
+import { TbAsteriskSimple } from "react-icons/tb";
+import Button from "../Common/Button";
+import { useState } from "react";
+import PropTypes from 'prop-types'
 
-const Verification = () => {
+const Verification = ({ setIsVerificationPopupOpen}) => {
+  const [verificationProgress, setVerificationProgress] = useState(false);
+
   return (
-    <div>Verification</div>
-  )
-}
+    <div className="relative rounded-xl bg-white p-36">
+      <div className=" w-[500px] flex flex-col items-center gap-10">
+        <img src="/src/assets/ver-image.png" width={60} />
+        <p className=" text-sm text-primaryTextColor text-center">
+          Two-step verification is on. You’ll need to enter your PIN if you
+          register your phone number on LOGO
+        </p>
+        <hr className=" border border-lineColor w-full" />
+        <div>
+          <h4 className=" text-sm text-secondaryTextColor mb-7">
+            {verificationProgress
+              ? "Enter your 6-digit PIN"
+              : "Create a 6-digit PIN that you can remember"}
+          </h4>
 
-export default Verification
+          <div className=" border-b-2 -lineColor flex justify-center items-center gap-3">
+            <div className="flex">
+              <div className="relative w-7 h-10">
+                <input
+                  type="password"
+                  className="bg-transparent w-full h-full text-center text-lg text-primaryTextColor font-medium outline-none"
+                />
+                <TbAsteriskSimple className=" text-lg text-primaryTextColor absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20" />
+              </div>
+              <div className="relative w-7 h-10">
+                <input
+                  type="password"
+                  className="bg-transparent w-full h-full text-center text-lg text-primaryTextColor font-medium outline-none"
+                />
+                <TbAsteriskSimple className=" text-lg text-primaryTextColor absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20" />
+              </div>
+              <div className="relative w-7 h-10">
+                <input
+                  type="password"
+                  className="bg-transparent w-full h-full text-center text-lg text-primaryTextColor font-medium outline-none"
+                />
+                <TbAsteriskSimple className=" text-lg text-primaryTextColor absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20" />
+              </div>
+            </div>
+            <div className="flex">
+              <div className="relative w-7 h-10">
+                <input
+                  type="password"
+                  className="bg-transparent w-full h-full text-center text-lg text-primaryTextColor font-medium outline-none"
+                />
+                <TbAsteriskSimple className=" text-lg text-primaryTextColor absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20" />
+              </div>
+              <div className="relative w-7 h-10">
+                <input
+                  type="password"
+                  className="bg-transparent w-full h-full text-center text-lg text-primaryTextColor font-medium outline-none"
+                />
+                <TbAsteriskSimple className=" text-lg text-primaryTextColor absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20" />
+              </div>
+              <div className="relative w-7 h-10">
+                <input
+                  type="password"
+                  className="bg-transparent w-full h-full text-center text-lg text-primaryTextColor font-medium outline-none"
+                />
+                <TbAsteriskSimple className=" text-lg text-primaryTextColor absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20" />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* pagination buttons */}
+        <div className="flex items-center gap-2">
+          <div
+            className={` h-2 w-2 rounded-full ${
+              !verificationProgress ? "bg-pGreen" : "bg-secondaryTextColor2"
+            } `}
+            onClick={() => setVerificationProgress(false)}
+          ></div>
+          <div
+            className={` h-2 w-2 rounded-full cursor-pointer ${
+              verificationProgress ? "bg-pGreen" : "bg-secondaryTextColor2"
+            } `}
+          ></div>{" "}
+        </div>
+        <Button
+          label={!verificationProgress?"Next":'Save'}
+          bstyles={"bg-pGreen text-white rounded-md text-sm py-2 px-5"}
+          handleClick={() => setVerificationProgress(true)}
+        />{" "}
+      </div>
+
+      <LiaTimesCircleSolid
+        className=" text-pRed text-3xl absolute top-7 right-7 cursor-pointer"
+        onClick={() => setIsVerificationPopupOpen(false)}
+      />
+    </div>
+  );
+};
+
+Verification.propTypes ={
+  isVericationPopupOpen: PropTypes.bool,
+  setIsVerificationPopupOpen: PropTypes.func
+}
+export default Verification;
