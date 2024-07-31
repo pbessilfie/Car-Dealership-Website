d8382fc622b48a149877b7f25901f2ec29186fb1
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-07-31 01:39:43 +0000

    passing the number of cards to be rendered as props

--- src/Components/Car/Recommended.jsx
+++ src/Components/Car/Recommended.jsx
@@ -1,20 +1,34 @@
 import { Cars } from "../../constants";
 import Car_Card from "../Cards/Car_Card";
-
-const Recommended = () => {
+import PropTypes from "prop-types";
+const Recommended = ({ end }) => {
   return (
     <div className="">
       <h3 className=" text-primaryTextColor font-bold text-xl mb-2">
         Recommended
       </h3>
-      <p className="text-sm text-secondaryTextColor font-normal mb-9">Discover all recommended offers</p>
+      <p className="text-sm text-secondaryTextColor font-normal mb-9">
+        Discover all recommended offers
+      </p>
       <div className="flex gap-5">
-        {Cars.slice(0,4).map((car,index)=>(
-            <Car_Card key={index} carName={car.Carname} price={car.price} condition={car.condition} production_year={car.production_year} reviews={car.reviews} brand={car.brand} mileage={car.mileage} category={car.category}/>
+        {Cars.slice(0,  end ).map((car, index) => (
+          <Car_Card
+            key={index}
+            carName={car.Carname}
+            price={car.price}
+            condition={car.condition}
+            production_year={car.production_year}
+            reviews={car.reviews}
+            brand={car.brand}
+            mileage={car.mileage}
+            category={car.category}
+          />
         ))}
       </div>
     </div>
   );
-}
-
-export default Recommended
+};
+Recommended.propTypes = {
+  end: PropTypes.number,
+};
+export default Recommended;
