9bb7df782556896d8760479a46bb14e9cd71ee72
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-08-01 04:07:46 +0000

    change the animation speed

--- src/Components/Car/CarReview.jsx
+++ src/Components/Car/CarReview.jsx
@@ -16,7 +16,7 @@ const CarReview = () => {
       <motion.div
         initial={{ x: 0 }}
         animate={{ x: "-195%" }}
-        transition={{ duration: 300, repeat: Infinity, ease: "linear" }}
+        transition={{ duration: 100, repeat: Infinity, ease: "linear" }}
         className="flex gap-7"
       >
         {C_Reviews.map((review, index) => (
