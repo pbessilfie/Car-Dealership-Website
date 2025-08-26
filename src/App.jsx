a8f8e458420573a12b31ded07ed1aebcfe634910
pbessilfie <pbessilfie001@st.ug.edu.gh>
2025-08-26 22:31:24 +0000

    wrap carsearchprovider around app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { Outlet } from "react-router";
 import Navbar from "./Components/Common/Navbar";
 import Footer from "./Components/Common/Footer";
+import CarSearchProvider from "./context/car-search-context";
 
 function App() {
   return (
-    <main
-      className=" font-lato overflow-hidden
-    "
-    >
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </main>
+    <CarSearchProvider>
+      <main className=" font-lato overflow-hidden">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </main>
+    </CarSearchProvider>
   );
 }
 
