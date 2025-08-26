4902aeaf007cd8a196d0886c80ff0868e17c7bc4
pbessilfie <pbessilfie001@st.ug.edu.gh>
2025-08-26 22:29:49 +0000

    add images data for the landing page carousel

---
+++ src/constants/swiper-images.js
@@ -0,0 +1,11 @@
+import swiperImage1 from "/src/assets/unsplash_HrbMyWmS7yU.png";
+import swiperImage2 from "/src/assets/unsplash_iw0D_9wHnf4.png";
+import swiperImage3 from "/src/assets/unsplash_v9ARc6COwfg.png";
+import swiperImage4 from "/src/assets/unsplash_9E1o3CsYmm0.png";
+
+export const swiperImages = [
+  swiperImage1,
+  swiperImage2,
+  swiperImage3,
+  swiperImage4,
+];
