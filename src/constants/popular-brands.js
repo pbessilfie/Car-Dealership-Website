ffd5d76a5f786e274414ee307e7dfc3c29a05a58
pbessilfie <pbessilfie001@st.ug.edu.gh>
2025-08-26 22:30:05 +0000

    feat: add popular car brands data

---
+++ src/constants/popular-brands.js
@@ -0,0 +1,50 @@
+import brandLogo1 from '/src/assets/bmw_of_north_america_logo.svg.png'
+import brandLogo2 from "/src/assets/toyota_symbol.svg.png";
+import brandLogo3 from "/src/assets/Group.png";
+import brandLogo4 from "/src/assets/mercedes-benz_symbol.svg.png";
+import brandLogo5 from "/src/assets/hyundai_symbol.svg.png";
+import brandLogo6 from "/src/assets/audi_logo.svg.png";
+import brandLogo7 from "/src/assets/lexus_logo.svg.png";
+import brandLogo8 from "/src/assets/honda_symbol.svg.png";
+export const popularBrands = [
+  {
+    brand: "BMW",
+    imgSrc: brandLogo1,
+    totalNumber: "129",
+  },
+  {
+    brand: "Toyota",
+    imgSrc: brandLogo2,
+    totalNumber: "300",
+  },
+  {
+    brand: "KIA",
+    imgSrc: brandLogo3,
+    totalNumber: "180",
+  },
+  {
+    brand: "Mercedes-Benz",
+    imgSrc: brandLogo4,
+    totalNumber: "150",
+  },
+  {
+    brand: "Hyundai",
+    imgSrc: brandLogo5,
+    totalNumber: "200",
+  },
+  {
+    brand: "Audi",
+    imgSrc: brandLogo6,
+    totalNumber: "180",
+  },
+  {
+    brand: "Lexus",
+    imgSrc: brandLogo7,
+    totalNumber: "220",
+  },
+  {
+    brand: "Honda",
+    imgSrc: brandLogo8,
+    totalNumber: "250",
+  },
+];
